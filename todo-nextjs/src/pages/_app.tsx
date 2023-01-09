16bfd07d010f57ef6bf187097eddd45f3a9bdacf
MohsinAliSoomro <MohsinAliSoomro23@gmail.com>
2023-01-10 00:32:06 +0500

    configure trpc

---
+++ todo-nextjs/src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from "next/app";
+import { trpc } from "../../utils/trpc";
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+export default trpc.withTRPC(App);
