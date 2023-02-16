b3232e2858b8c9b257b7847ada328526a980c805
MohsinAliSoomro <MohsinAliSoomro23@gmail.com>
2023-02-16 13:11:29 +0500

    design contact form

--- portfolio/pages/contact.tsx
+++ portfolio/pages/contact.tsx
@@ -1,3 +1,18 @@
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="flex items-center justify-center w-full h-[90vh]">
+      <form className="flex flex-col space-y-5 border rounded-lg p-4 w-full lg:w-3/12 text-secondary1 ">
+        <p className="text-2xl text-center text-">Hire me </p>
+        <div >
+          <label className="">Email</label>
+          <input placeholder="Email"  className="w-full rounded p-2 flex text-secondary3 bg-transparent border outline-none" />
+        </div>
+        <div>
+          <label>Message</label>
+          <textarea placeholder="Write Message" rows={4} className="w-full rounded p-2 flex text-secondary3 bg-transparent border outline-none" />
+        </div>
+        <button className="w-full rounded p-2 text-center border">Submit</button>
+      </form>
+    </div>
+  );
 }
